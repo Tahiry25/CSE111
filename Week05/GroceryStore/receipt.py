ddd14359ee19f972b8e2e3d8f44bf4ed383a162f
Tahiry Ratsimanohatra <kamaray.za@gmail.com>
2023-12-03 00:04:21 -0700

    sub

--- Week05/GroceryStore/receipt.py
+++ Week05/GroceryStore/receipt.py
@@ -1,22 +1,49 @@
 import csv
+from datetime import datetime
 
 PRODUCT_INDEX = 0
+SALES_TAX = 0.06
+DISCOUNT = 0.10
+STORE_NAME = "Alibaba Local Store"
 
 def main():
+    date = datetime.now()
+    current_date = date.strftime("%a %b %d %I:%M:S %Y")
     product_dict = read_dictionary('products.csv', PRODUCT_INDEX)
-    print("All Products")
-    print("------------")
-    print(f"{product_dict}\n")
-    print("Requested Items")
-    print("---------------")
+
+    print(STORE_NAME)
+    print("-------------------\n")
+    items_total_qty = 0
+    sub_total = 0
     with open('request.csv', 'rt') as file:
         reader = csv.reader(file)
         next(reader)
         for row_list in reader:
-            product_name = product_dict[row_list[0]][1]
-            qty = row_list[1]
-            price = product_dict[row_list[0]][2]
-            print(f"{product_name}: {qty} @ {price}")
+            try:
+                product_name = product_dict[row_list[0]][1]
+                qty = int(row_list[1])
+                items_total_qty += qty
+                price = float(product_dict[row_list[0]][2])
+                sub_total += qty * price
+                print(f"{product_name}: {qty} @ {price}")
+            except:
+                print("Unkown product ID in the request.csv file")
+
+    tax = sub_total * SALES_TAX
+    discount = sub_total * DISCOUNT
+    if date.strftime("%a") == "Tue":
+        total = sub_total + tax - discount
+    elif date.strftime("%a") == "Wed":
+        total = sub_total + tax - discount
+    else:
+        total = sub_total + tax
+
+    print(f"\nNumber of items: {items_total_qty}")
+    print(f"Subtotal: {sub_total:.2f}")
+    print(f"Sales Tax: {tax:.2f}")
+    print(f"Total: {total:.2f}")
+    print(f"\nThank you for shopping at {STORE_NAME}.")
+    print(current_date)
 
 
 def read_dictionary(filename, key_column_index):
@@ -31,14 +58,17 @@ def read_dictionary(filename, key_column_index):
         the contents of the CSV file.
     """
     dictionary = {}
-    with open(filename, "rt") as file:
-        reader = csv.reader(file)
-        next(reader)
+    try:
+        with open(filename, "rt") as file:
+            reader = csv.reader(file)
+            next(reader)
 
-        for row in reader:
-            key = row[key_column_index]
-            dictionary[key] = row
-    return dictionary
+            for row in reader:
+                key = row[key_column_index]
+                dictionary[key] = row
+        return dictionary
+    except:
+        print("File not found")
 
 # Call main to start this program.
 if __name__ == "__main__":
---
+++ Week05/GroceryStore/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+date = datetime.now()
