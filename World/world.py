fbb08c1827711743d5a96c9435032d6587e16be3
Tahiry-MO <tratsimanohatra@medonegroup.com>
2023-12-07 17:00:08 -0700

    Update world.py

--- World/world.py
+++ World/world.py
@@ -1,19 +1,19 @@
 import mysql.connector
 import tkinter as tk
 
-window = tk.Tk()
-window.geometry("300x400")
-window.title("City Finder")
-label = tk.Label(window, text="Enter name of a city", font=('Arial', 20))
-label.pack(padx=50, pady=(10,5))
-textbox = tk.Entry(window, font=('Arial', 16))
-textbox.pack(pady=10, padx=20)
-button = tk.Button(window, text="Get City Details")
-button.pack()
+# window = tk.Tk()
+# window.geometry("300x400")
+# window.title("City Finder")
+# label = tk.Label(window, text="Enter name of a city", font=('Arial', 20))
+# label.pack(padx=50, pady=(10,5))
+# textbox = tk.Entry(window, font=('Arial', 16))
+# textbox.pack(pady=10, padx=20)
+# button = tk.Button(window, text="Get City Details")
+# button.pack()
 
-button.bind("<Button-1>", print('test'))
+# button.bind("<Button-1>", print('test'))
 
-window.mainloop()
+# window.mainloop()
 
 
 db = mysql.connector.connect(
@@ -29,21 +29,22 @@ def main():
     city_name = input('City to Find: ')
     try:
         city_details = get_city_details(city_name)
-        print(city_details)
+        continent = get_continent(city_details[0])
+        capital = get_capital(city_details[3])
+        print(f"\n{city_details[4]} is located in {city_details[1]}, {continent}.")
+        print(f"This city has a population of at least {city_details[2]}.")
+        print(f"The capital of {city_details[1]} is {capital}.")
+        languages = get_languages(city_details[0])
+        print(f"In {city_details[1]}, they speak: ")
+        for l in languages:
+            print(f" - {l[0]}")
     except:
         print('Sorry, city not found.')
 
-    try:
-        capital = get_capital(city_details[3])
-        print(capital)
-    except:
-        print('Country not found.')
-
 
 # Retrieve city details from database (country code, country name, population, capitalId)
-def get_city_details(event):
-    city = textbox.get()
-    query = f"SELECT country.code, country.name, country.population, country.capital FROM world.city INNER JOIN country ON city.CountryCode = country.Code WHERE city.name = '{city}'"
+def get_city_details(city):
+    query = f"SELECT country.code, country.name, city.population, country.capital, city.name FROM world.city INNER JOIN country ON city.CountryCode = country.Code WHERE city.name LIKE '%{city}%'"
     cursor.execute(query)
     city_details = cursor.fetchall()
     return city_details[0]
@@ -55,6 +56,18 @@ def get_capital(code):
     capital = cursor.fetchall()
     return capital[0][0]
 
+def get_languages(countryCode):
+    query = f"SELECT countrylanguage.Language FROM world.countrylanguage INNER JOIN country ON countrylanguage.CountryCode = country.Code WHERE country.code = '{countryCode}'"
+    cursor.execute(query)
+    languages = cursor.fetchall()
+    return languages
+
+
+def get_continent(countryCode):
+    query = f"select country.continent FROM world.country WHERE country.code = '{countryCode}'"
+    cursor.execute(query)
+    continent = cursor.fetchall()
+    return continent[0][0]
 
 # Call main to start this program.
 if __name__ == "__main__":
